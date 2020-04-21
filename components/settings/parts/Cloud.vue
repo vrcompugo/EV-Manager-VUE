693d6562eb0abeed5fc31d8cd0ccdbf7519dcbc3
Andreas Hedderich <a.hedderich@hbundb.de>
2020-04-21 16:25:09 +0200

    re

--- components/settings/parts/Cloud.vue
+++ components/settings/parts/Cloud.vue
@@ -25,13 +25,53 @@
       </v-card>
       <v-btn @click="settings.cloud_prices.push({})"><v-icon>mdi-plus</v-icon></v-btn>
     </div>
+    <b>Laufzeiten</b>
+    <v-text-field
+      label="Kündigungsfrist"
+      v-model="settings.notice_period"></v-text-field>
+    <v-text-field
+      label="Vertragslaufzeit"
+      v-model="settings.contract_run_time"></v-text-field>
+    <v-text-field
+      label="garantierte Zero-Laufzeit für (a)"
+      v-model="settings.guaranteed_run_time"></v-text-field>
+    <br>
     <b>Consumer</b>
     <v-text-field
       label="Consumer Extra Basispreis"
       type="number"
       step="0.01"
       v-model="settings.consumer_base_cost"></v-text-field>
+
+    <b>Extra Produkte</b>
+    <div class="spaced-horizontal">
+      <v-card v-for="(product, index) in settings.extra_products" :key="product.label" style="margin-bottom: 1em;">
+        <div style="flex: 1">
+          <v-text-field
+            label="Bezeichnung"
+            v-model="product.label"></v-text-field>
+          <v-text-field
+            label="Sortierung"
+            type="number"
+            v-model="product.order"></v-text-field>
+          <v-text-field
+            label="Artikel"
+            v-model="product.product_link"></v-text-field>
+          <v-text-field
+            label="Artikelordner"
+            v-model="product.product_folder"></v-text-field>
+          Immer im Angebot hinzufügen?
+          <v-radio-group v-model="product.include_always" row>
+            <v-radio label="Als 1-Produkt" value="top-one"></v-radio>
+            <v-radio label="Als 0-Produkt" value="top"></v-radio>
+          </v-radio-group>
+        </div>
+        <v-btn @click="settings.extra_products.splice(index, 1)"><v-icon>mdi-delete</v-icon></v-btn>
+      </v-card>
+      <v-btn @click="settings.extra_products.push({})"><v-icon>mdi-plus</v-icon></v-btn>
+    </div>
   </div>
+
 </template>
 
 
