bdf05bd317207f68da6987d2c2727fb7f2f5c144
Andreas Hedderich <a.hedderich@hbundb.de>
2020-11-12 19:15:13 +0100

    KESO-92

--- pages/quote_calculator/_id.vue
+++ pages/quote_calculator/_id.vue
@@ -1783,6 +1783,9 @@
 
     <div>
       <div class="actions">
+        <v-btn :href="`/vue/quote_calculator/${id}?token=${$route.query.token}`" target="_blank" style="margin-right: 1em">
+          <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path d="M0 0h24v24H0z" fill="none"/><path d="M7 14H5v5h5v-2H7v-3zm-2-4h2V7h3V5H5v5zm12 7h-3v2h5v-5h-2v3zM14 5v2h3v3h2V5h-5z"/></svg>
+        </v-btn>
         <v-btn @click="reloadProducts" style="margin-right: 1em">P</v-btn>
         <div class="kwp-bar layout horizontal" v-if="data.has_pv_quote">
           <div
