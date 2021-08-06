6cac84b810dfe0dd443d753d6907b9394db8f036
Andreas Hedderich <a.hedderich@hbundb.de>
2021-08-06 18:32:59 +0200

    cloud refresh hidden

--- pages/quote_calculator/_id.vue
+++ pages/quote_calculator/_id.vue
@@ -259,7 +259,7 @@
                                   item-text="label"
                                   item-value="value"></v-select>
                               </div>
-                              <div>
+                              <!--<div>
                                 <h3>Cloud.Refresh</h3>
                                 <v-checkbox
                                   label="Kunde hat Altanlage"
@@ -301,7 +301,7 @@
                                     suffix="kWp"
                                     style="width: 26em; margin-right: 1em"></v-text-field>
                                 </div>
-                              </div>
+                              </div>-->
                             </div>
 
                             <h3>Extra Pakete</h3>
