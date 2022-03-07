07358e112d2022a6df10174a949ca598d2206dc1
Andreas Hedderich <a.hedderich@hbundb.de>
2022-03-07 11:50:37 +0100

    fullscreen

--- pages/cloud/contract/_year/list.vue
+++ pages/cloud/contract/_year/list.vue
@@ -5,6 +5,9 @@
 <template>
   <div>
     <h1>Cloud Abrechnung {{ year }}</h1>
+    <v-btn :href="`/vue/cloud/contract/${year}/list?token=${$route.query.token}`" target="_blank" style="margin-right: 1em">
+      <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path d="M0 0h24v24H0z" fill="none"/><path d="M7 14H5v5h5v-2H7v-3zm-2-4h2V7h3V5H5v5zm12 7h-3v2h5v-5h-2v3zM14 5v2h3v3h2V5h-5z"/></svg>
+    </v-btn>
     <v-btn @click="checkAllContractData()" small>Check All</v-btn>
     <v-btn @click="generateAllContracts()" small>Generate All</v-btn>
     <table cellspacing="0">
