3a529b55f611b4ab263055e594cef1228cea367c
Andreas Hedderich <a.hedderich@hbundb.de>
2023-01-25 12:38:43 +0100

    rework

---
+++ components/quote_calculator/quotes/cloud/financing.vue
@@ -0,0 +1,125 @@
+<template>
+  <div class="layout horizontal wrap">
+    <div class="section" style="padding-bottom: 0">
+      <div class="section">
+        <h2>Finanzierung</h2>
+        <div class="layout horizontal">
+          <v-select
+            v-model="data.investment_type" :items="[
+              {'value':'financing','label':'Finanzierung'},
+              {'value':'cash','label':'Barkauf'}
+            ]"
+            @input="formChanged"
+            style="flex: 0 1 10em; margin-left: 1em;"
+            item-text="label"
+            item-value="value"></v-select>
+          <v-select
+            v-if="data.investment_type == 'financing'"
+            v-model="data.financing_bank" :items="[
+              {'value':'energie360','label':'über Energie360'},
+              {'value':'ownbank','label':'Hausbank'}
+            ]"
+            @input="calculateCloud"
+            style="flex: 0 1 10em; margin-left: 1em;"
+            item-text="label"
+            item-value="value"></v-select>
+          <v-text-field
+            v-if="data.investment_type == 'financing'"
+            v-model="data.loan_upfront"
+            @input="formChanged"
+            label="Anzahlung"
+            suffix="€"
+            style="flex: 0 0 12em; margin-left: 1em; margin-right: 1em"></v-text-field>
+        </div>
+        <div class="flex" v-if="data.investment_type == 'financing'">
+          <v-slider
+            :disabled="data.financing_bank == 'energie360'"
+            v-model="data.financing_rate"
+            @input="formChanged"
+            min="0"
+            max="8"
+            step="0.01"
+            thumb-label="always"></v-slider>
+          <div style="margin-top: -1em; padding-left: 0.5em">Finanzierungs Zinssatz</div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+
+<script>
+import {formatNumber, formatPrice} from '~/plugins/formatNumber'
+
+export default {
+
+  components: {
+  },
+
+  props: [
+    'id',
+    'data',
+    'calculated',
+    'select_options',
+    'possible_storage_sizes',
+    'pv_modules_selections'
+  ],
+
+  computed: {
+    checkCloudRights: function () { return this.$store.getters['quote_calculator/checkCloudRights'] },
+    checkBookkeepingRights: function () { return this.$store.getters['quote_calculator/checkBookkeepingRights'] },
+    checkMitteRights: function () { return this.$store.getters['quote_calculator/checkMitteRights'] }
+  },
+
+  data(){
+    return {
+      stepper: 1,
+      histories__: [],
+      editId: 0,
+      rules: {
+        requiredBoolean: value => value !== undefined || 'Pflichteingabe',
+        required:  value => !!value || 'Pflichteingabe',
+      }
+    }
+  },
+
+  mounted () {
+  },
+
+  methods: {
+    validate () {
+      this.$nextTick(() => {
+        let found = false
+        for (let field in this.$refs) {
+          let element = this.$refs[field]
+          if(Array.isArray(element)){
+            element = element[0]
+          }
+          if(element !== undefined && element._isVue && !element.validate(true)){
+            found = true
+          }
+        }
+        this.roofs[this.index].is_valid = !found
+      })
+    },
+    emitInput(){
+      this.$emit('input', {})
+    },
+    calculateCloud () {
+      this.$emit('calculateCloud')
+    },
+    formatNumber() {
+      // legacy
+      return formatNumber()
+    },
+    formatPrice() {
+      // legacy
+      return formatPrice()
+    },
+    formChanged () {
+      // legacy
+    }
+  }
+
+}
+</script>
---
+++ components/quote_calculator/quotes/cloud/usage.vue
@@ -0,0 +1,252 @@
+<template>
+  <div>
+    <div class="layout horizontal wrap">
+      <div class="section" style="padding-bottom: 0">
+        <h3>Verbrauch Lichtstrom</h3>
+        <small>&nbsp;</small>
+        <v-text-field
+          ref="power_usage"
+          v-model="data.power_usage"
+          :rules="[rules.required]"
+          @keyup.enter="calculateCloud"
+          @blur="calculateCloud"
+          label="Verbrauch in kWh"
+          class="align-right"
+          suffix="kWh"
+          type="number"
+          step="1"></v-text-field>
+        <v-text-field
+          v-if="checkCloudRights || checkBookkeepingRights"
+          v-model="data.power_extra_usage"
+          @keyup.enter="calculateCloud"
+          @blur="calculateCloud"
+          label="Extra-Verbrauch in kWh"
+          class="align-right"
+          suffix="kWh"
+          type="number"
+          step="1"></v-text-field>
+        <small>mehr kWh werden mit {{ formatNumber(calculated.lightcloud_extra_price_per_kwh * 100, 2) }} Cent kWh abgerechnet</small><br>
+      </div>
+      <div class="section" :disabled="true" style="padding-bottom: 0">
+        <h3>Wärmecloud</h3>
+        <small><b>Voraussetzung:</b> 2ter Zähler / Wärmezähler und Konzept 8</small>
+        <v-text-field :disabled="data.has_heating_quote" v-model="data.heater_usage" @keyup.enter="calculateCloud" @blur="calculateCloud" label="Verbrauch in kWh" class="align-right" suffix="kWh" type="number" step="1"></v-text-field>
+        <small>mehr kWh werden mit {{ formatNumber(calculated.heatcloud_extra_price_per_kwh * 100, 2) }} Cent abgerechnet</small>
+      </div>
+      <div class="section" :disabled="true" style="padding-bottom: 0">
+        <h3>E.Cloud</h3>
+        <small><b>Voraussetzung:</b> Gasheizung</small>
+        <v-text-field :disabled="true" v-model="data.ecloud_usage" @keyup.enter="calculateCloud" @blur="calculateCloud" label="Gas Verbrauch in kWh" class="align-right" suffix="kWh" type="number" step="1"></v-text-field>
+        <small>mehr kWh werden mit {{ formatNumber(calculated.ecloud_extra_price_per_kwh * 100, 2) }} Cent kWh Gas abgerechnet</small>
+      </div>
+    </div>
+    <br />
+
+    <h3>Cloud Optionen</h3>
+    <div class="layout horizontal wrap">
+      <div style="margin-right: 1em">
+        <v-radio-group v-model="data.cloud_quote_type" @change="calculateCloud" style="margin-top: 0.3em">
+          <v-radio
+            value=""
+            label="Standard" />
+          <v-radio
+            value="no-cloud"
+            label="Keine Cloud" />
+          <v-radio
+            disabled
+            value="combination_quote"
+            label="Cloud Kombinationsvertrag (nur 2 Jahre Laufzeit)" />
+          <v-radio
+            v-if="checkCloudRights || checkBookkeepingRights"
+            value="followup_quote"
+            label="Cloud Folgeangebot (nur 1 Jahr Laufzeit)" />
+          <v-radio
+            v-if="checkCloudRights || checkBookkeepingRights"
+            value="interim_quote"
+            label="Cloud Zwischenangebot (nur 1 Jahr Laufzeit)" />
+        </v-radio-group>
+      </div>
+      <div style="margin-right: 1em">
+        <v-select
+          v-if="checkCloudRights || checkBookkeepingRights"
+          label="Cloud Laufzeit"
+          v-model="data.price_guarantee" :items="[
+            {'value':'12_years','label':'12 Jahre'},
+            {'value':'10_years','label':'10 Jahre'},
+            {'value':'2_years','label':'2 Jahre'},
+            {'value':'1_year','label':'1 Jahr'}
+          ]"
+          @input="calculateCloud"
+          style="max-width: 14em;"
+          item-text="label"
+          item-value="value"></v-select>
+        <v-select
+          v-else
+          label="Cloud Laufzeit"
+          v-model="data.price_guarantee" :items="[
+            {'value':'12_years','label':'12 Jahre'},
+            {'value':'2_years','label':'2 Jahre'}
+          ]"
+          @input="calculateCloud"
+          style="max-width: 14em;"
+          item-text="label"
+          item-value="value"></v-select>
+      </div>
+      <div v-if="checkCloudRights || checkBookkeepingRights">
+        <v-radio-group v-model="data.old_price_calculation" @change="calculateCloud" style="margin-top: 0.3em">
+          <v-radio
+            value=""
+            label="Standard" />
+          <v-radio
+            value="l2k3fblk3baxv55"
+            label="Preisdefintion vor dem 16.12.2021" />
+          <v-radio
+            value="5EQnMQSgTFhK3AlS"
+            label="Preisdefintion vor dem 28.02.2022" />
+          <v-radio
+            value="VOgcqFFeQLpV9cxOA02lzXdAYX"
+            label="Preisdefintion vor dem 01.10.2022" />
+          <v-radio
+            value="CXRsAMcrJw7V9wTA4L5ELE8xJx9NVNo9"
+            label="Preisdefintion vor dem 15.10.2022" />
+          <v-radio
+            value="oTv8BT9RzG3ms7QJcq7Y"
+            label="Preisdefintion vor dem 25.02.2022" />
+        </v-radio-group>
+        <v-checkbox
+          v-model="data.cloud_quote_heat_seperate"
+          label="Wärmecloud gesondert ausweisen" />
+      </div>
+      <!--
+      <div>
+        <h3>Cloud.Refresh</h3>
+        <v-checkbox
+          label="Kunde hat Altanlage"
+          style="margin-right: 1em"
+          @change="calculateCloud"
+          v-model="data.has_old_pv" />
+        <div v-if="data.has_old_pv">
+          <v-menu
+            ref="datepickerMenu"
+            v-model="datepickerMenu"
+            :close-on-content-click="false"
+            transition="scale-transition"
+            offset-y
+            min-width="auto"
+          >
+            <template v-slot:activator="{ on, attrs }">
+              <v-text-field
+                v-model="data.old_pv_build_date"
+                type="date"
+                label="Wann ist die Anlage in Betrieb gegangen"
+                prepend-icon="mdi-calendar"
+                readonly
+                v-bind="attrs"
+                v-on="on"
+              ></v-text-field>
+            </template>
+            <v-date-picker
+              v-model="data.old_pv_build_date"
+              :active-picker.sync="activePicker"
+              min="1950-01-01"
+              @change="datePickerSave"
+            ></v-date-picker>
+          </v-menu>
+          <v-text-field
+            v-model="data.old_pv_kwp"
+            @blur="calculateCloud"
+            label="Nennleistung der alten Anlage laut Einspeisevertrag"
+            type="number"
+            suffix="kWp"
+            style="width: 26em; margin-right: 1em"></v-text-field>
+        </div>
+      </div>-->
+    </div>
+  </div>
+</template>
+
+
+<script>
+import {formatNumber, formatPrice} from '~/plugins/formatNumber'
+
+export default {
+
+  components: {
+  },
+
+  props: [
+    'id',
+    'data',
+    'calculated',
+    'select_options',
+    'possible_storage_sizes',
+    'pv_modules_selections'
+  ],
+
+  computed: {
+    checkCloudRights: function () { return this.$store.getters['quote_calculator/checkCloudRights'] },
+    checkBookkeepingRights: function () { return this.$store.getters['quote_calculator/checkBookkeepingRights'] },
+    checkMitteRights: function () { return this.$store.getters['quote_calculator/checkMitteRights'] }
+  },
+
+  data(){
+    return {
+      stepper: 1,
+      histories__: [],
+      editId: 0,
+      rules: {
+        requiredBoolean: value => value !== undefined || 'Pflichteingabe',
+        required:  value => !!value || 'Pflichteingabe',
+      }
+    }
+  },
+
+  mounted () {
+  },
+
+  methods: {
+    validate () {
+      this.$nextTick(() => {
+        let found = false
+        for (let field in this.$refs) {
+          let element = this.$refs[field]
+          if(Array.isArray(element)){
+            element = element[0]
+          }
+          if(element !== undefined && element._isVue && !element.validate(true)){
+            found = true
+          }
+        }
+        this.roofs[this.index].is_valid = !found
+      })
+    },
+    addRoof() {
+      this.data.roofs.push({ direction: 'west_east' })
+      if (this.data.roofs.length > 3){
+        if (this.data.extra_options.indexOf('solaredge') < 0) {
+          this.data.extra_options.push('solaredge')
+        }
+      }
+      this.calculateCloud()
+    },
+    emitInput(){
+      this.$emit('input', {})
+    },
+    calculateCloud () {
+      this.$emit('calculateCloud')
+    },
+    formatNumber() {
+      // legacy
+      return formatNumber()
+    },
+    formatPrice() {
+      // legacy
+      return formatPrice()
+    },
+    formChanged () {
+      // legacy
+    }
+  }
+
+}
+</script>
